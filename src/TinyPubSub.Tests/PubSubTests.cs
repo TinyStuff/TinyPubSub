3198f695d98aaf0d690dcff40a0c04e9b433c681
Johan Karlsson <johan.kson@gmail.com>
2017-08-20 19:40:35 +0200

    Feature xunit (#8)
    * Added ConfigureAwait(false) to PublishAsTask to avoid ThreadAbortionException
    * Converted tests to xunit

--- src/TinyPubSub.Tests/PubSubTests.cs
+++ src/TinyPubSub.Tests/PubSubTests.cs
@@ -1,130 +1,128 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TinyPubSub.Tests
 {
-    [TestClass]
     public class PubSubTests
     {
-        [TestInitialize]
-        public void InitializeTests()
-        {
-            TinyPubSubLib.TinyPubSub.Clear();
-        }
+		public PubSubTests()
+		{
+			TinyPubSubLib.TinyPubSub.Clear();
+		}
 
-        [TestMethod]
-        public void SubscribeWithArgumentTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = (x == "duck"));
+		[Fact]
+		public void SubscribeWithArgumentTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = (x == "duck"));
 
-            // Act
-            TinyPubSubLib.TinyPubSub.Publish("test", "duck");
+			// Act
+			TinyPubSubLib.TinyPubSub.Publish("test", "duck");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+            Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public void SubscribeWithArgumentMissingTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
+		[Fact]
+		public void SubscribeWithArgumentMissingTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
 
-            // Act
-            TinyPubSubLib.TinyPubSub.Publish("test", "dumbargument");
+			// Act
+			TinyPubSubLib.TinyPubSub.Publish("test", "dumbargument");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public void SubscribeWithArgumentMissingButArgumentedSubscriptionTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = (x == null));
+		[Fact]
+		public void SubscribeWithArgumentMissingButArgumentedSubscriptionTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = (x == null));
 
-            // Act
-            TinyPubSubLib.TinyPubSub.Publish("test");
+			// Act
+			TinyPubSubLib.TinyPubSub.Publish("test");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public void SubscribePublishTheMostCommonWayTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
+		[Fact]
+		public void SubscribePublishTheMostCommonWayTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
 
-            // Act
-            TinyPubSubLib.TinyPubSub.Publish("test");
+			// Act
+			TinyPubSubLib.TinyPubSub.Publish("test");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public async Task DelayedSubscribePublishTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
+		[Fact]
+		public async Task DelayedSubscribePublishTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
 
-            // Act
-            TinyPubSubLib.TinyPubSub.PublishAsTask("test");
-            await Task.Delay(100);
+			// Act
+			TinyPubSubLib.TinyPubSub.PublishAsTask("test");
+			await Task.Delay(100);
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public void DelayedSubscribePublishNotWaitingForCompletionTest()
-        {
-            // Arrange
-            var testsuccessful = true;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = false);
+		[Fact]
+		public void DelayedSubscribePublishNotWaitingForCompletionTest()
+		{
+			// Arrange
+			var testsuccessful = true;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = false);
 
-            // Act
-            TinyPubSubLib.TinyPubSub.PublishAsTask("test");
+			// Act
+			TinyPubSubLib.TinyPubSub.PublishAsTask("test");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public async Task DelayedSubscribePublishWithArgumentsTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = x == "duck");
+		[Fact]
+		public async Task DelayedSubscribePublishWithArgumentsTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", (x) => testsuccessful = x == "duck");
 
-            // Act
-            TinyPubSubLib.TinyPubSub.PublishAsTask("test", "duck");
-            await Task.Delay(100);
+			// Act
+			TinyPubSubLib.TinyPubSub.PublishAsTask("test", "duck");
+			await Task.Delay(100);
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
 
-        [TestMethod]
-        public async Task PublishAsyncTest()
-        {
-            // Arrange
-            var testsuccessful = false;
-            TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
+		[Fact]
+		public async Task PublishAsyncTest()
+		{
+			// Arrange
+			var testsuccessful = false;
+			TinyPubSubLib.TinyPubSub.Subscribe("test", () => testsuccessful = true);
 
-            // Act
-            await TinyPubSubLib.TinyPubSub.PublishAsync("test");
+			// Act
+			await TinyPubSubLib.TinyPubSub.PublishAsync("test");
 
-            // Assert
-            Assert.IsTrue(testsuccessful);
-        }
+			// Assert
+			Assert.True(testsuccessful);
+		}
     }
 }
